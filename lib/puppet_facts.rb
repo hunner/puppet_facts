cf3df349a2ce28482438f1f1b62a9cc911357d3f
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-11-27 15:47:52 +0100

    Add methods to retrieve non-PE platforms
    * Add on_puppet_supported_platforms and on_puppet_unsupported_platforms
    * Refactor to use common code
    * Simplify code for existing methods
    * Add documentation

--- lib/puppet_facts.rb
+++ lib/puppet_facts.rb
@@ -18,43 +18,74 @@ module PuppetFacts
     PuppetFacts.pe_platforms[pe_version]
   end
 
-  def on_pe_supported_platforms(targets=nil)
-    targets = Array(targets) if targets
+  def on_pe_supported_platforms(targets='all')
+    on_supported_platforms('pe', 'PE', targets)
+  end
 
-    # TODO This should filter based on set_pe_supported_platforms
-    facts = PuppetFacts.pe_platform_facts
-    sup_facts = Hash.new
-    facts.each do |pe_ver,platforms|
-      pe_semver = "#{pe_ver.sub(/^PE/,'')}.0"
-      if SemVer[PuppetFacts.get_pe_requirement] === SemVer.new(pe_semver)
-        sup_facts[pe_ver] = platforms.select do |platform, facts|
-          if targets
-            PuppetFacts.meta_supported_platforms.include?(platform) && targets.include?(platform)
-          else
-            PuppetFacts.meta_supported_platforms.include?(platform)
-          end
-        end
-      end
-    end
-    sup_facts
+  def on_puppet_supported_platforms(targets='all')
+    on_supported_platforms('puppet', '', targets)
+  end
+
+  # Returns a hash of filtered supported platforms
+  #
+  # @param [String] type the type of Puppet installation ('pe' or 'puppet')
+  # @param [String] prefix the prefix for data directories (e.g. 'PE' or '')
+  # @param [Array] targets the targets to filter on
+  # @api public
+  def on_supported_platforms(type, prefix, targets='all')
+    on_platforms(true, type, prefix, targets='all')
+  end
+
+  def on_pe_unsupported_platforms(targets='all')
+    on_unsupported_platforms('pe', 'PE', targets)
+  end
+
+  def on_puppet_unsupported_platforms(targets='all')
+    on_unsupported_platforms('puppet', '', targets)
   end
 
   # We need the inverse, this is kind of ugly. I don't want to cram it into the
   # other method however.
-  def on_pe_unsupported_platforms(targets=nil)
-    targets = Array(targets) if targets
+  #
+  # Returns a hash of filtered unsupported platforms
+  #
+  # @param [String] type the type of Puppet installation ('pe' or 'puppet')
+  # @param [String] prefix the prefix for data directories (e.g. 'PE' or '')
+  # @param [Array] targets the targets to filter on
+  # @api public
+  def on_unsupported_platforms(type, prefix, targets='all')
+    on_platforms(false, type, prefix, targets='all')
+  end
+
+  # Returns a hash of filtered platforms
+  #
+  # @param [Boolean] supported filter on supported or unsupported platforms
+  # @param [String] type the type of Puppet installation ('pe' or 'puppet')
+  # @param [String] prefix the prefix for data directories (e.g. 'PE' or '')
+  # @param [Array] targets the targets to filter on
+  # @api public
+  def on_platforms(supported, type, prefix, targets)
+    targets = Array(targets)
 
     # TODO This should filter based on set_pe_supported_platforms
-    facts = PuppetFacts.pe_platform_facts
+    facts = PuppetFacts.platform_facts(type, prefix)
     sup_facts = Hash.new
-    facts.each do |pe_ver,platforms|
-      pe_semver = "#{pe_ver.sub(/^PE/,'')}.0"
-      if SemVer[PuppetFacts.get_pe_requirement] === SemVer.new(pe_semver)
-        sup_facts[pe_ver] = platforms.select do |platform, facts|
-          if targets
-            ! PuppetFacts.meta_supported_platforms.include?(platform) && ! targets.include?(platform)
+    facts.each do |ver,platforms|
+      semver = "#{ver.sub(/^#{prefix}/,'')}.0"
+      if SemVer[PuppetFacts.get_requirement(type)] === SemVer.new(semver)
+        sup_facts[ver] = platforms.select do |platform, facts|
+          if targets != ['all']
+            if supported
+              PuppetFacts.meta_supported_platforms.include?(platform) && targets.include?(platform)
+            else
+              ! PuppetFacts.meta_supported_platforms.include?(platform) && ! targets.include?(platform)
+            end
           else
-            ! PuppetFacts.meta_supported_platforms.include?(platform)
+            if supported
+              PuppetFacts.meta_supported_platforms.include?(platform)
+            else
+              ! PuppetFacts.meta_supported_platforms.include?(platform)
+            end
           end
         end
       end
@@ -69,7 +100,8 @@ module PuppetFacts
     fail(StandardError, "PE dir missing")
   end
 
-  @pe_dirs = Dir[File.join(@proj_root,"PE*")]
+  @data_dirs = Dir.glob(File.join(@proj_root, "/*")).select { |d| d =~ %r{/[^ \t/]*\d\.\d$} }
+  @pe_dirs = @data_dirs.select { |d| d =~ %r{/PE\d\.\d} }
 
   # Returns array of PE versions
   def self.pe_versions
@@ -84,51 +116,35 @@ module PuppetFacts
   end
 
   # @api private
-  def self.pe_platforms
-    @pe_platforms ||= get_pe_platforms
+  def self.facts_paths(type, prefix)
+    dir = @data_dirs.select { |d| d =~ %r{/#{prefix}\d\.\d} }
+    @facts_path ||= {}
+    @facts_path[type] ||= get_facts_paths(dir)
   end
 
   # @api private
-  def self.get_pe_platforms
-    @pe_dirs.inject({}) do |memo,pe_dir|
-      pe_version = File.basename(pe_dir)
-      if Dir[File.join(@proj_root,pe_version,"*")].empty?
-        fail(StandardError, "Puppet facts missing for #{pe_version}")
-      end
-      memo[pe_version] = Dir[File.join(@proj_root,pe_version,"*")].collect do |facts|
-        File.basename(facts.gsub(/\.facts/, ''))
-      end
-      memo
-    end
-  end
-
-  # @api private
-  def self.pe_facts_paths
-    @pe_facts_paths ||= get_pe_facts_paths
-  end
-
-  # @api private
-  def self.get_pe_facts_paths
-    @pe_dirs.collect do |dir|
+  def self.get_facts_paths(dir)
+    dir.collect do |dir|
       Dir[File.join(dir,"*")]
     end.flatten
   end
 
   # @api private
-  def self.pe_platform_facts
-    @pe_platform_facts ||= get_pe_platform_facts
+  def self.platform_facts(type, prefix)
+    @platform_facts ||= {}
+    @platform_facts[type] ||= get_platform_facts(type, prefix)
   end
 
   # @api private
-  def self.get_pe_platform_facts
-    pe_facts_paths.inject({}) do |memo,file|
-      pe_platform = File.basename(file.gsub(/\.facts/, ''))
-      pe_version = File.basename(File.dirname(file))
-      memo[pe_version] = Hash.new unless memo[pe_version]
-      memo[pe_version][pe_platform] = Hash.new
+  def self.get_platform_facts(type, prefix)
+    facts_paths(type, prefix).inject({}) do |memo,file|
+      platform = File.basename(file.gsub(/\.facts/, ''))
+      version = File.basename(File.dirname(file))
+      memo[version] = Hash.new unless memo[version]
+      memo[version][platform] = Hash.new
       File.read(file).each_line do |line|
         key, value = line.split(' => ')
-        memo[pe_version][pe_platform][key.to_sym] = value.chomp unless value.nil?
+        memo[version][platform][key.to_sym] = value.chomp unless value.nil?
       end
       memo
     end
@@ -196,17 +212,17 @@ module PuppetFacts
   end
 
   # @api private
-  def self.get_pe_requirement
+  def self.get_requirement(type='pe')
     metadata = get_metadata
     if metadata['requirements'].nil?
       fail StandardError, 'No requirements in metadata'
     end
-    pe_requirement = metadata['requirements'].select do |x|
-      x['name'] == 'pe'
+    requirement = metadata['requirements'].select do |x|
+      x['name'] == type
     end
-    if pe_requirement.empty?
-      fail StandardError, 'No PE requirement found in metadata'
+    if requirement.empty?
+      fail StandardError, "No #{type} requirement found in metadata"
     end
-    pe_requirement.first['version_requirement']
+    requirement.first['version_requirement']
   end
 end
